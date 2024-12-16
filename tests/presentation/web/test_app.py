af00a2df33ec75f325035a2ae2705e84d7ef626c
Jakub Svehla <jakub.svehla@gmail.com>
2024-12-16 13:48:19 +0100

    Add a sample test

---
+++ tests/presentation/web/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app.bootstrap import bootstrap
+from app.presentation.web import WebApp
+
+
+@pytest.mark.asyncio
+async def test_health_endpoint() -> None:
+    container = bootstrap()
+    app = container[WebApp]
+
+    client = app.test_client()
+    response = await client.get("/health")
+    assert response.status_code == 200
