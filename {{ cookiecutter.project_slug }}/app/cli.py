227ec6bb451d4c565b696a6609b9a74b2b277506
Jakub Svehla <jakub.svehla@gmail.com>
2024-12-19 22:01:18 +0100

    Update how the CLI app is started

--- {{ cookiecutter.project_slug }}/app/cli.py
+++ {{ cookiecutter.project_slug }}/app/cli.py
@@ -1,10 +1,18 @@
+import asyncio
+
 from app.presentation.cli import CLIApp
 
 from .bootstrap import bootstrap
 
-container = bootstrap()
 
-app = container[CLIApp]
+async def main() -> None:
+    container = bootstrap()
+
+    app = container[CLIApp]
+
+    await container.init()
+    await app.run()
+
 
 if __name__ == "__main__":
-    app.run()
+    asyncio.run(main())
